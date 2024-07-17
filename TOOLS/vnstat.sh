d79e00cfacf46f3430afd61b7f7c5372563ce003
remours <samyangganss@gmail.com>
2024-07-17 22:57:17 +0700

    Update vnstat.sh

--- TOOLS/vnstat.sh
+++ TOOLS/vnstat.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # READ AUTH
-if [ -f "/root/TgBotWRT/AUTH" ]; then
-    BOT_TOKEN=$(head -n 1 /root/TgBotWRT/AUTH)
-    CHAT_ID=$(tail -n 1 /root/TgBotWRT/AUTH)
+if [ -f "/root/TeleWRT/AUTH" ]; then
+    BOT_TOKEN=$(head -n 1 /root/TeleWRT/AUTH)
+    CHAT_ID=$(tail -n 1 /root/TeleWRT/AUTH)
 else
     echo "Berkas AUTH tidak ditemukan."
     exit 1
@@ -18,6 +18,4 @@ MSG="
 "
 
 # Mengirim pesan ke akun Telegram pribadi
-URL="https://tgbotwrt.titit.tech/bot.jpg"
-curl -F "chat_id=$CHAT_ID" -F "caption=$MSG" -F "photo=$URL" \
-https://api.telegram.org/bot$BOT_TOKEN/sendphoto
+curl -F "chat_id=$CHAT_ID" -F "caption=$MSG" \
