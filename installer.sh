9702f0984a04b8ec1e1c0ee035cb024281b2f8ea
remours <samyangganss@gmail.com>
2024-07-17 22:43:23 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -57,38 +57,26 @@ sleep 2
 pip3 install telepot requests python-telegram-bot
 opkg list-installed | grep python3
 pip3 list
-git clone -b SCRIPT https://github.com/EdyDevz/TgBotWRT
-mv /root/TgBotWRT/edy /etc/init.d/
-mv /root/TgBotWRT/edy.py /usr/bin/
+git clone -b SCRIPT https://github.com/remours/TeleWRT
+mv /root/TeleWRT/edy /etc/init.d/
+mv /root/TeleWRT/edy.py /usr/bin/
 chmod +x /etc/init.d/edy
 chmod +x /usr/bin/edy.py
-chmod +x /root/TgBotWRT/*
-chmod +x /root/TgBotWRT/TOOLS/*
+chmod +x /root/TeleWRT/*
+chmod +x /root/TeleWRT/TOOLS/*
 clear
 echo "SUCCESS"
 clear
 sleep 5
-echo "JANGAN LUPA ISI BOT TOKEN DAN ID TELEGRAM YA ANJING"
-sleep 10
-echo "TUTORIAL DAH LENGKAP JANGAN BANYAK TANYA YA ANJING"
+echo "JANGAN LUPA ISI BOT TOKEN DAN ID TELEGRAM YA "
 sleep 10
 clear
-nano /root/TgBotWRT/AUTH
+nano /root/TeleWRT/AUTH
 echo "PLEASE WAIT....."
 sleep 5
 clear
 echo "SETUP TOOLS ....."
 sleep 5
 clear
-echo "
-╔════════════════════════════════════════════════════════╗
- ███████╗██████╗░██╗░░░██╗░██╗░░░░░░░██╗██████╗░████████╗
- ██╔════╝██╔══██╗╚██╗░██╔╝░██║░░██╗░░██║██╔══██╗╚══██╔══╝
- █████╗░░██║░░██║░╚████╔╝░░╚██╗████╗██╔╝██████╔╝░░░██║░░░
- ██╔══╝░░██║░░██║░░╚██╔╝░░░░████╔═████║░██╔══██╗░░░██║░░░
- ███████╗██████╔╝░░░██║░░░░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░
- ╚══════╝╚═════╝░░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░
-╚════════════════════════════════════════════════════════╝"
-echo "[+] THAKS FOR USE MY TOOLS & SUPPORT ME :)"
 service edy enable
 service edy start
