67ce93f55ae67195c9daa287582beb436da08e74
remours <samyangganss@gmail.com>
2024-07-17 22:54:37 +0700

    Update ocstart.sh

--- TOOLS/ocstart.sh
+++ TOOLS/ocstart.sh
@@ -15,6 +15,4 @@ fi
 MSG="❏ OC STARTED ❏"
 
 # Mengirim pesan ke akun Telegram pribadi
-URL="https://tgbotwrt.titit.tech/bot.jpg"
-curl -F "chat_id=$CHAT_ID" -F "caption=$MSG" -F "photo=$URL" \
-https://api.telegram.org/bot$BOT_TOKEN/sendphoto
+curl -F "chat_id=$CHAT_ID" -F "caption=$MSG" \
